6e7d9ee88ce3e6f7616eea667798d6fe0455dd88
RamanPatsiayuk <meloman1987@gmail.com>
2015-10-20 00:58:50 +0300

    added javaapp-test module

--- JavaWebTask/javaapp-service/src/main/resources/department-data-script.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Department VALUES(1, 'Java', 'Brest');
-INSERT INTO Department VALUES(2, 'Javascript', 'Brest');
-INSERT INTO Department VALUES(3, 'Groovy', 'Brest');
-INSERT INTO Department VALUES(4, '.Net', 'Brest');
-INSERT INTO Department VALUES(5, 'Python', 'Brest');
-INSERT INTO Department VALUES(6, 'PHP', 'Brest');
